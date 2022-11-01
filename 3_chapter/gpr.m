98d500872ed8afbe2ff4c6f8d9eeb03541fb0ef6
Your Name <you@example.com>
2022-11-01 12:48:41 +0900

    create kv function

--- 3_chapter/gpr.m
+++ 3_chapter/gpr.m
@@ -9,13 +9,22 @@ sigma = log(1);
 eta = log(1);
 params = [tau sigma eta];
 
-% 
+% 学習データ読み込み
+data = readmatrix('gpr.dat');
+xtrain = zeros(length(data), 1); ytrain = zeros(length(data), 1);
+for i = 1:1:length(data)
+    xtrain(i,1) = data(i,1);
+    ytrain(i,1) = data(i,2);
+end
 
 
 
-
+% 計算ゾーン
 x = 1; y = 2;
 gaussian_kernel(x, y, params);
+kv(x, xtrain, params)
+
+
 
 % kgauss(x, y);
 function delta = delta(x, y)
@@ -50,16 +59,19 @@ function kgrad = kgauss_grad(xi, xj, d, kernel, params)
     end
 end
 
-% 一つのxtrainに対する行ベクトル．すなわち行が入力xx (１次元の入力ならx軸(横軸))，列がxtrainの学習カーネル行列？？
-function kv = kv(x, xtrain, kernel)
-    for i = 1:1:N
+% 一つの入力xに対するxtrainの列ベクトル．すなわち列が入力x (１次元の入力ならx軸(横軸))，行がxtrainの学習カーネル行列？？
+% xtrainは列ベクトル（縦）
+function kv = kv(x, xtrain, params) % kernelを無名関数で作ってないから，kernelとして引数で与えられないことに注意．
+    kv = zeros(length(xtrain), 1);
+    for i = 1:1:length(xtrain)
+        kv(i,1) = gaussian_kernel(x, xtrain(i,1), params);
+    end    
 end
 
 
 
-
-
-
+% function ans = main()
+% end
 
 
 
